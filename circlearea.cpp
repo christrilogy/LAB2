93a219cbf3003b863ce89c6961fe3399952057bd
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-02-01 16:07:22 -0500

    Create circlearea.cpp

---
+++ circlearea.cpp
@@ -0,0 +1,16 @@
+// This program will write the name, address and telephone
+// number of the programmer.
+// Christian Morgan
+
+#include <iostream>
+  using namespace std;
+  int main()
+  {
+  cout << "Christian Morgan"<< endl;
+  cout <<"123 Markadella Lane"<< endl;
+  cout <<"Fruitland, Md. 55503 \n \n \n"<< endl;
+
+  cout <<"489-555-5555"<< endl;
+
+  return 0;
+  }
