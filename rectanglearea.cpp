1bc9f9300160170b0739d74c28483fdb8240f8b7
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-02-01 16:14:36 -0500

    Create rectanglearea.cpp

---
+++ rectanglearea.cpp
@@ -0,0 +1,27 @@
+
+    // This program will output the perimeter and area
+    // of the rectangle.
+    // Christian Morgan
+    #include <iostream>
+    using namespace std;
+
+    const double LENGTH = 8;
+    const double WIDTH = 3;
+
+    int main() {
+      float area;                      // definition of area of rectangle
+      int perimeter;               // definition of perimeter of a rectangle
+      perimeter =  2 * (LENGTH + WIDTH); // computes perimeter
+      area = LENGTH * WIDTH ;   // computes area
+
+      cout << "The perimeter of the rectangle is " << perimeter << "\n";
+      // Fill in the code for the cout statement that will output (with description)
+
+      // the circumference
+      cout << "The area of the rectangle is " << area << "";
+      // Fill in the code for the cout statement that will output (with description)
+
+      // the area of the circle
+
+      return 0;
+    }
